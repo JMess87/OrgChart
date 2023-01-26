9418baec5d8bdfa1873d9af90196239f79adc66c
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-01-25 21:58:04 -0500

    added read me, adjusted index.js

--- index.js
+++ index.js
@@ -2,28 +2,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 inquirer
     .prompt([
+        // manager questions
         {
             type: 'input',
-            name: 'title',
-            message: 'What is your project title?',
+            name: 'manager_name',
+            message: 'Manager Name?',
         },
         {
             type: 'input',
-            name: 'description',
-            message: 'What is your project description?',
+            name: 'employee_id',
+            message: 'Enter employee ID?',
         },
         {
             type: 'input',
-            name: 'installation',
-            message: 'Do you have an installation instructions?',
+            name: 'email',
+            message: 'Enter email.',
         },
         {
             type: 'input',
-            name: 'usage',
-            message: 'What is your usage information?',
+            name: 'office_number',
+            message: 'Enter office phone number?',
         },
+        {
+            type: 'input',
+            name: 'trigger_engineer',
+            message: 'Would you like to add an engineer?',
+        },
+
+        // Engineer Questions - stopped here for the night
         {
             type: 'list',
             name: 'license',
@@ -62,10 +71,32 @@ inquirer
             console.log("You have an error!!!!!!!")
             // Prompt couldn't be rendered in the current environment
         } else {
-            console.log("Blue Screen of Death !!!!")
+            console.log("HTML FILE FAILED TO RENDER!!!!")
             // Something else went wrong
         }
     });
-// below -  generation of the read me file, layout of the md language to create the read me. 
+// below -  generation of the html file with the information populate from the information input in the command line.
 const generateHtml = ({ title, description, installation, usage, license, contribution, test, github, questions }) =>
+    `<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
     
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="card" style="width: 18rem;">
+<div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Test</a>
+</div>
+</div>
+    </body
+    </html>`
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Module 10 Read Me - Project Org Chart
+
+##  Description
+Create a node.js command line application that takes in information about employees on a software engineering team, then generate an HTML webpage that displays summaries for each person. 
+
+### Checklist of contents 
+
+1. Manager
+    a. Name
+    b. Employee ID
+    c. email address 
+    d. office number
+2. Engineer
+    a. Name 
+    b. Employee ID
+    c. Email
+    d. GitHub username
+
+3. Intern 
+    a. Name
+    b. ID
+    c. Email
+    d. School
+
+## Link to video of walk through
+
+Used base code for file creation from my Module 9 assignment - CreateReadMe from the command prompt.
+
+## Resources and Credits
+
